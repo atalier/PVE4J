9ba8e32cef0da88750bd6cad0649bac4f72ebd5d
Aceitix <aceitix@hotmail.com>
2026-03-30 16:44:21 +0200

    Add PveQemuAgentNetworkInterface.toString()

--- src/main/java/fr/freshperf/pve4j/entities/nodes/node/qemu/agent/networkInterface/PveQemuAgentNetworkInterface.java
+++ src/main/java/fr/freshperf/pve4j/entities/nodes/node/qemu/agent/networkInterface/PveQemuAgentNetworkInterface.java
@@ -2,6 +2,8 @@ package fr.freshperf.pve4j.entities.nodes.node.qemu.agent.networkInterface;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Arrays;
+
 public class PveQemuAgentNetworkInterface {
     private String name;
     @SerializedName("hardware-address")
@@ -14,5 +16,15 @@ public class PveQemuAgentNetworkInterface {
     public String getHardwareAddress() { return hardwareAddress; }
     public PveQemuAgentNetworkIpAddress[] getIpAddresses() { return ipAddresses; }
     public PveQemuAgentNetworkStatistics getStatistics() { return statistics; }
+
+    @Override
+    public String toString() {
+        return "PveQemuAgentNetworkInterface{" +
+                "name='" + name + '\'' +
+                ", hardwareAddress='" + hardwareAddress + '\'' +
+                ", ipAddresses=" + Arrays.toString(ipAddresses) +
+                ", statistics=" + statistics +
+                '}';
+    }
 
 }
